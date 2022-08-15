ec31cb2c005c570a1512f93a0a573af4dd82c1ad
Zubair2002 <94192905+Zubair2002@users.noreply.github.com>
2022-08-15 16:13:56 -0400

    Update API.js

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -6,7 +6,7 @@ var baseURL;
 // } else {
 //     baseURL = 'http://127.0.0.1:8000';
 // }
-baseURL = 'https://backend-moviereview.herokuapp.com/'
+baseURL = 'https://demo-backend-pro.herokuapp.com/'
 
 const api = axios.create({
     baseURL: baseURL,
